30accde46b99b601490a4ed22f86313a8c41ecd3
Andrew Firsenko <andrewfirsenko@gmail.com>
2025-05-24 23:18:30 +0300

    Create project

---
+++ CountryQuiz/CountryQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryQuizApp.swift
+//  CountryQuiz
+//
+//  Created by Andrew on 24.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
